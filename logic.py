154f698f9aab124a4977dad9e7e8b88e5bb7b63d
T-Money555 <tmonaghan-sova@unomaha.edu>
2025-05-08 14:42:44 -0500

    Fixed logic so all buttons work correctly

--- logic.py
+++ logic.py
@@ -21,6 +21,7 @@ class Logic(QMainWindow, Ui_remote_menu):
         self.vol_slider.setEnabled(False)
 
         self.power.clicked.connect(lambda: self.tv_power())
+        self.mute.clicked.connect(lambda: self.mute_tv())
         self.num_1.clicked.connect(lambda: self.channel_1())
         self.num_2.clicked.connect(lambda: self.channel_2())
         self.num_3.clicked.connect(lambda: self.channel_3())
@@ -31,6 +32,9 @@ class Logic(QMainWindow, Ui_remote_menu):
         self.num_8.clicked.connect(lambda: self.channel_8())
         self.num_9.clicked.connect(lambda: self.channel_9())
         self.ch_up.clicked.connect(lambda: self.channel_up())
+        self.ch_down.clicked.connect(lambda: self.channel_down())
+        self.vol_up.clicked.connect(lambda: self.volume_up())
+        self.vol_down.clicked.connect(lambda: self.volume_down())
         self.vol_slider.valueChanged.connect(lambda: self.slider())
 
         self.__status: bool = False
@@ -47,11 +51,11 @@ class Logic(QMainWindow, Ui_remote_menu):
             self.check_channel()
             self.vol_slider.setEnabled(True)
         else:
-            self.__status = False
             self.label_power.setPixmap(QtGui.QPixmap("Pictures/black.png"))
+            self.__status = False
             self.vol_slider.setEnabled(False)
 
-    def mute(self) -> None:
+    def mute_tv(self) -> None:
         """
         Method to mute or unmute the tv
         """
@@ -67,64 +71,73 @@ class Logic(QMainWindow, Ui_remote_menu):
         """
         changes to channel 1
         """
-        self.label_power.setPixmap(QtGui.QPixmap("Pictures/nbc.png"))
-        self.__channel = 1
+        if self.__status:
+            self.label_power.setPixmap(QtGui.QPixmap("Pictures/nbc.png"))
+            self.__channel = 1
 
     def channel_2(self) -> None:
         """
         changes to channel 2
         """
-        self.label_power.setPixmap(QtGui.QPixmap("Pictures/cn.png"))
-        self.__channel = 2
+        if self.__status:
+            self.label_power.setPixmap(QtGui.QPixmap("Pictures/cn.png"))
+            self.__channel = 2
 
     def channel_3(self) -> None:
         """
         changes to channel 3
         """
-        self.label_power.setPixmap(QtGui.QPixmap("Pictures/abc.png"))
-        self.__channel = 3
+        if self.__status:
+            self.label_power.setPixmap(QtGui.QPixmap("Pictures/abc.png"))
+            self.__channel = 3
 
     def channel_4(self) -> None:
         """
         changes to channel 4
         """
-        self.label_power.setPixmap(QtGui.QPixmap("Pictures/fox_news.png"))
-        self.__channel = 4
+        if self.__status:
+            self.label_power.setPixmap(QtGui.QPixmap("Pictures/fox_news.png"))
+            self.__channel = 4
 
     def channel_5(self) -> None:
         """
         changes to channel 5
         """
-        self.label_power.setPixmap(QtGui.QPixmap("Pictures/food_network.png"))
-        self.__channel = 5
+        if self.__status:
+            self.label_power.setPixmap(QtGui.QPixmap("Pictures/food_network.png"))
+            self.__channel = 5
 
     def channel_6(self) -> None:
         """
         changes to channel 6
         """
-        self.label_power.setPixmap(QtGui.QPixmap("Pictures/hgtv.png"))
-        self.__channel = 6
+        if self.__status:
+            self.label_power.setPixmap(QtGui.QPixmap("Pictures/hgtv.png"))
+            self.__channel = 6
 
     def channel_7(self) -> None:
         """
         changes to channel 7
         """
-        self.label_power.setPixmap(QtGui.QPixmap("Pictures/hallmark.png"))
-        self.__channel = 7
+        if self.__status:
+            self.label_power.setPixmap(QtGui.QPixmap("Pictures/hallmark.png"))
+            self.__channel = 7
 
     def channel_8(self) -> None:
         """
         changes to channel 8
         """
-        self.label_power.setPixmap(QtGui.QPixmap("Pictures/history.png"))
-        self.__channel = 8
+        if self.__status:
+            self.label_power.setPixmap(QtGui.QPixmap("Pictures/history.png"))
+            self.__channel = 8
 
     def channel_9(self) -> None:
         """
         changes to channel 9
         """
-        self.label_power.setPixmap(QtGui.QPixmap("Pictures/espn.png"))
-        self.__channel = 9
+        if self.__status:
+            self.label_power.setPixmap(QtGui.QPixmap("Pictures/espn.png"))
+            self.__channel = 9
 
     def channel_up(self) -> None:
         """
@@ -135,7 +148,7 @@ class Logic(QMainWindow, Ui_remote_menu):
                 self.__channel += 1
             else:
                 self.__channel = Logic.MIN_CHANNEL
-        self.check_channel()
+            self.check_channel()
 
     def channel_down(self) -> None:
         """
@@ -146,7 +159,7 @@ class Logic(QMainWindow, Ui_remote_menu):
                 self.__channel -= 1
             else:
                 self.__channel = Logic.MAX_CHANNEL
-        self.check_channel()
+            self.check_channel()
 
     def check_channel(self) -> None:
         """
@@ -187,8 +200,7 @@ class Logic(QMainWindow, Ui_remote_menu):
             self.vol_slider.setEnabled(True)
             if self.__volume < Logic.MAX_VOLUME:
                 self.__volume += 1
-
-        self.vol_slider.setTickPosition(self.__volume)
+            self.vol_slider.setValue(self.__volume)
 
     def volume_down(self) -> None:
         """
@@ -199,5 +211,4 @@ class Logic(QMainWindow, Ui_remote_menu):
             self.vol_slider.setEnabled(True)
             if self.__volume > Logic.MIN_VOLUME:
                 self.__volume -= 1
-
-        self.vol_slider.setTickPosition(self.__volume)
+            self.vol_slider.setValue(self.__volume)
